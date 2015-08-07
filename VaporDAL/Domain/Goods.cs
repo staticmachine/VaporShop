e5a9d94e6e5056bcaac3782730d60ea3b0eb6494
staticmachine <iwantpieceandlove@gmail.com>
2015-08-07 22:16:34 +0400

    *

---
+++ VaporDAL/Domain/Goods.cs
@@ -0,0 +1,33 @@
+﻿namespace VaporDAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The goods.
+    /// </summary>
+    public class Goods
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type id.
+        /// </summary>
+        public int TypeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the brand id.
+        /// </summary>
+        public int BrandId { get; set; }
+    }
+}
